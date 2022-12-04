85f0cdde0be4fe57a58eb2c6d3c2039a711c3345
Igor Maliy <imaliy.filsh@gmail.com>
2022-12-04 23:58:14 +0200

    Development

--- src/Handler/ChannelHandler.php
+++ src/Handler/ChannelHandler.php
@@ -11,7 +11,6 @@ use Mailery\Sender\Email\Entity\EmailSender;
 use Mailery\Channel\Smtp\Mailer\MailerFactory;
 use Mailery\Channel\Smtp\Mailer\MessageFactory;
 use Mailery\Template\Renderer\Context;
-use Mailery\Template\Renderer\ContextObserver;
 use Mailery\Template\Email\Renderer\WrappedUrlGenerator;
 use Yiisoft\Yii\Cycle\Data\Writer\EntityWriter;
 use Yiisoft\Router\UrlGeneratorInterface;
@@ -63,14 +62,11 @@ class ChannelHandler implements HandlerInterface
         (new EntityWriter($this->entityManager))->write([$recipient]);
 
         try {
-            $observer = new ContextObserver();
-            $context = (new Context([
-                'url' => new WrappedUrlGenerator($this->urlGenerator, $recipient),
-            ]))->withObserver($observer);
-
             $mailer = $this->mailerFactory->create($sender->getChannel());
             $message = $this->messageFactory
-                ->withContext($context)
+                ->withContext(new Context([
+                    'url' => new WrappedUrlGenerator($this->urlGenerator, $recipient),
+                ]))
                 ->create($campaign, $recipient);
 
             $mailer->send($message);
