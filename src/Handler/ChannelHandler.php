6fc8b21e9dc424474e9d0e2c37f3569f8bef40c5
Igor Maliy <imaliy.filsh@gmail.com>
2022-12-16 00:19:53 +0200

    Development

--- src/Handler/ChannelHandler.php
+++ src/Handler/ChannelHandler.php
@@ -3,24 +3,21 @@
 namespace Mailery\Channel\Amazon\Ses\Handler;
 
 use Cycle\ORM\EntityManagerInterface;
-use Mailery\Campaign\Entity\Campaign;
 use Mailery\Campaign\Entity\Sendout;
 use Mailery\Campaign\Entity\Recipient;
 use Mailery\Campaign\Renderer\WrappedUrlGenerator;
 use Mailery\Channel\Handler\HandlerInterface;
-use Mailery\Sender\Email\Entity\EmailSender;
 use Mailery\Channel\Smtp\Mailer\MailerFactory;
 use Mailery\Channel\Smtp\Mailer\MessageFactory;
-use Mailery\Template\Renderer\Context;
-use Yiisoft\Yii\Cycle\Data\Writer\EntityWriter;
+use Mailery\Channel\Smtp\Handler\ChannelHandler as SmtpChannelHandler;
 
 class ChannelHandler implements HandlerInterface
 {
 
     /**
-     * @var bool
+     * @var SmtpChannelHandler
      */
-    private bool $suppressErrors = false;
+    private SmtpChannelHandler $handler;
 
     /**
      * @param MailerFactory $mailerFactory
@@ -29,21 +26,25 @@ class ChannelHandler implements HandlerInterface
      * @param WrappedUrlGenerator $wrappedUrlGenerator
      */
     public function __construct(
-        private MailerFactory $mailerFactory,
-        private MessageFactory $messageFactory,
-        private EntityManagerInterface $entityManager,
-        private WrappedUrlGenerator $wrappedUrlGenerator
-    ) {}
+        MailerFactory $mailerFactory,
+        MessageFactory $messageFactory,
+        EntityManagerInterface $entityManager,
+        WrappedUrlGenerator $wrappedUrlGenerator
+    ) {
+        $this->handler = new SmtpChannelHandler(
+            $mailerFactory,
+            $messageFactory,
+            $entityManager,
+            $wrappedUrlGenerator
+        );
+    }
 
     /**
      * @inheritdoc
      */
-    public function withSuppressErrors(bool $suppressErrors): self
+    public function withSuppressErrors(bool $suppressErrors): HandlerInterface
     {
-        $new = clone $this;
-        $new->suppressErrors = $suppressErrors;
-
-        return $new;
+        return $this->handler->withSuppressErrors($suppressErrors);
     }
 
     /**
@@ -51,52 +52,7 @@ class ChannelHandler implements HandlerInterface
      */
     public function handle(Sendout $sendout, Recipient $recipient): bool
     {
-        /** @var Campaign $campaign */
-        $campaign = $sendout->getCampaign();
-
-        /** @var EmailSender $sender */
-        $sender = $campaign->getSender();
-
-        $recipient->setSendout($sendout);
-        (new EntityWriter($this->entityManager))->write([$recipient]);
-
-        $message = $this->messageFactory
-            ->withContext($this->createContext($recipient))
-            ->create($recipient);
-
-        $recipient->setMessageContext($message->getContext());
-
-        try {
-            $sentMessage = $this->mailerFactory
-                ->createTransport($sender->getChannel())
-                ->send($message);
-
-            $recipient->setSent(true);
-            $recipient->setMessageId($sentMessage->getMessageId());
-        } catch (\Exception $e) {
-            $recipient->setError($e->getMessage());
-            throw $e;
-        } finally {
-            (new EntityWriter($this->entityManager))->write([$recipient]);
-        }
-
-        return true;
-    }
-
-    /**
-     * @param Recipient $recipient
-     * @return Context
-     */
-    private function createContext(Recipient $recipient): Context
-    {
-        $wrappedUrlGenerator = $this->wrappedUrlGenerator->withRecipient($recipient);
-        if (($subscriber = $recipient->getSubscriber()) !== null) {
-            $wrappedUrlGenerator = $wrappedUrlGenerator->withSubscriber($subscriber);
-        }
-
-        return new Context([
-            'url' => $wrappedUrlGenerator,
-        ]);
+        return $this->handler->handle($sendout, $recipient);
     }
 
 }
